9d3f9184e98edc57cbe25d3f2d62b478dd85eb24
John Azariah <john.azariah@gmail.com>
2017-01-17 16:46:12 +0000

    Add text -> code; sign assembly

---
+++ src/FSM.Orleans/CodeGenerator.fs
@@ -0,0 +1,12 @@
+﻿namespace FSM.Orleans
+
+open BrightSword.RoslynWrapper
+
+[<AutoOpen>]
+module CodeGenerator =
+    let insert_namespace_into_compilation_unit ns = ``compilation unit`` [ns]
+
+    let generate_code_for_text =
+        text_to_valid_machine >> build_namespace >> insert_namespace_into_compilation_unit >> generateCodeToString
+
+
